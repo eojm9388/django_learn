96d7742552df960cf72623682099ad7cce48fd70
어지민 <eojm9388@gmail.com>
2024-03-27 17:59:12 +0900

    django-form 실습c

---
+++ form/django_ws_6_c/todos/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Todo
+
+
+class TodoForm(forms.ModelForm):
+
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+        widgets = {
+            'is_completed': forms.CheckboxInput(
+                attrs={
+                    'type': 'hidden'
+                } 
+            )
+        }
